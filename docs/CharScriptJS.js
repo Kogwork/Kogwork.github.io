640b3afdff54f74847e75b0fdaf143488a105e0e
Alex Breger <b0558008@mail.sapir.ac.il>
2021-05-09 21:02:03 +0300

    required field + text length in contact check update to use JS

--- docs/CharScriptJS.js
+++ docs/CharScriptJS.js
@@ -1,11 +1,9 @@
-
-
 //skills
 const barData = {"cpp": 95, "git": 80, "Full-Stack": 50, "Java": 80, "DevOps": 75}
 
 const barsByClass = document.getElementsByClassName("progress")
 
-for (let i = 0; i<barsByClass.length; i++){
+for (let i = 0; i < barsByClass.length; i++) {
     let element = barsByClass.item(i)
     element.addEventListener("mouseover", complete)
 }
@@ -19,16 +17,30 @@ function complete() {
 
 //contact me
 
-function checkLength(){
+function checkLength() {
     var textArea = document.getElementById("contactText"),
         count = textArea.value.length,
-        maxLength = textArea.maxLength,
+        maxLength = 200,
         remaining = maxLength - count,
-        charCount = document.getElementById("charcount_text")
-    if(remaining <= 0) {
-        charCount.innerHTML = maxLength + ' too much words is UN-BASED'
+        charCount = document.getElementById("charcount_text"),
+        submitBtn = document.getElementById("submitBtn")
+
+    if (textArea.value.length > maxLength) {
+        textArea.value = textArea.value.substring(0, maxLength)
+        charCount.innerHTML = "exceeded " + maxLength + 'chars. Too much words is UN-BASED'
     } else {
         charCount.innerHTML = 'chars ' + remaining + ' left'
+    }
+}
+
+//Required field
+function required() {
+    var button = document.getElementById("submitBtn")
+    var email = document.forms["contactForm"]["Email"].value
+    var firstN = document.forms["contactForm"]["First name"].value
+    var lastN = document.forms["contactForm"]["Last name"].value
+    if (email == "" || firstN == "" || lastN == "") {
+        return false;
     }
 }
 
@@ -36,6 +48,7 @@ function checkLength(){
 
 // When the user clicks on the button, scroll to the top of the document
 document.getElementById("backToTop").addEventListener("click", topFunction)
+
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
@@ -43,17 +56,19 @@ function topFunction() {
 
 
 // Modal
-    var modal = document.getElementById("chadModal");
+var modal = document.getElementById("chadModal");
 
-    var img = document.getElementById("profilePic");
-    var modalImg = document.getElementById("jumpImg");
-    img.onclick = function () {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    }
+var img = document.getElementById("profilePic");
+var modalImg = document.getElementById("jumpImg");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
 
-    var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+
